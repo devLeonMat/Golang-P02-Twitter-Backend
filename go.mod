08ed35283b97a61436f9415667da4a9cefbcd1a0
Leon Matias <roslin.pineda@nautilus.team>
2021-11-16 22:51:11 -0500

    add project

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/leonmatias2015/Golang-P02-Twitter-Backend
 
 go 1.17
 
-require go.mongodb.org/mongo-driver v1.7.3
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.0
+	go.mongodb.org/mongo-driver v1.7.3
+)
 
 require (
 	github.com/go-stack/stack v1.8.0 // indirect
--- main..go
+++ main..go
@@ -1,5 +1,15 @@
 package Golang_P02_Twitter_Backend
 
-func main() {
+import (
+	"github.com/leonmatias2015/Golang-P02-Twitter-Backend/bd"
+	"github.com/leonmatias2015/Golang-P02-Twitter-Backend/handlers"
+	"log"
+)
 
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("Without connection to database")
+		return
+	}
+	handlers.Managers()
 }
